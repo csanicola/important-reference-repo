6197868053be56626bf933778d693dfff5b23797
Caroline Sanicola <80606894+csanicola@users.noreply.github.com>
2026-03-25 16:25:51 -0400

    Update sql_for_data_analysis.sql

--- Online_Courses/Udacity/SQL_for_Data_Analysis/sql_for_data_analysis.sql
+++ Online_Courses/Udacity/SQL_for_Data_Analysis/sql_for_data_analysis.sql
@@ -1787,6 +1787,165 @@ SELECT AVG(avg_amt)
 	FROM t2;
 
 
+-- SQL Data Cleaning
+/*
+-- EXAMPLE:
+SELECT first_name,
+		last_name,
+		phone_number,
+		LEFT(phone_number, 3) AS area_code,
+		RIGHT(phone_number, 8) AS phone_numnber_only,
+		RIGHT(phone_number, LENGTH(phone_number) - 4) AS phone_number_alt
+	FROM customer_data
+*/
+
+-- RIGHT & LEFT
+/*
+In the accounts table, there is a column holding the website for each company. 
+The last three digits specify what type of web address they are using. 
+A list of extensions (and pricing) is provided here(opens in a new tab). 
+Pull these extensions and provide how many of each website type exist in the accounts table.
+*/
+WITH t1 AS (SELECT RIGHT(website, 3) AS web_extension
+				FROM accounts)
+SELECT web_extension, 
+		COUNT(*) 
+	FROM t1
+	GROUP BY 1;
+		
+/*
+There is much debate about how much the name (or even the first letter of a company name)(opens in a new tab) matters. 
+Use the accounts table to pull the first letter of each company name to see the distribution of company names that begin with each letter (or number).
+*/
+SELECT LEFT(UPPER(name), 1) AS account_first_letter,
+		COUNT(*) num_companies
+	FROM accounts
+	GROUP BY account_first_letter
+	ORDER BY account_first_letter;
+
+/*
+Use the accounts table and a CASE statement to create two groups: 
+one group of company names that start with a number and a second group of those company names that start with a letter. 
+What proportion of company names start with a letter?
+*/
+SELECT SUM(num) nums, 
+		SUM(letter) letters
+	FROM (SELECT name,
+				CASE WHEN LEFT(UPPER(name), 1) IN ('0', '1', '2', '3', '4', '5', '6', '7', '8', '9')
+						THEN 1 ELSE 0 END AS num,
+				CASE WHEN LEFT(UPPER(name), 1) IN ('0', '1', '2', '3', '4', '5', '6', '7', '8', '9')
+						THEN 0 ELSE 1 END AS letter
+			FROM accounts) t1;
+
+/*
+Consider vowels as a, e, i, o, and u. 
+What proportion of company names start with a vowel, and what percent start with anything else?
+*/
+SELECT SUM(vowel) vowels,
+		SUM(consonant) consonants
+	FROM (SELECT name,
+				CASE WHEN LEFT(UPPER(name), 1) IN ('A', 'E', 'I', 'O', 'U')
+					THEN 1 ELSE 0 END AS vowel,
+				CASE WHEN LEFT(UPPER(name), 1) IN ('A', 'E', 'I', 'O', 'U')
+					THEN 0 ELSE 1 END AS consonant
+			FROM accounts) t1;
+
+-- POSITION, STRPOS, SUBSTR
+-- Example:
+/*
+SELECT first_name,
+		last_name,
+		city_state,
+		POSITION(',' IN city_state) AS comma_position,
+		STRPOS(city_state, ',') AS substr_comma_position,
+		LOWER(city_state) AS lowercase,
+		UPPER(city_state) AS uppercase,
+		LEFT(city_state, POSITION(',' IN city_state)) AS city
+	FROM customer_data;
+*/
+
+-- Questions
+/*
+Use the accounts table to create first and last name columns that hold the first and last names for the primary_poc.
+*/
+SELECT LEFT(primary_poc, STRPOS(primary_poc, ' ') -1) AS first_name,
+		RIGHT(primary_poc, LENGTH(primary_poc) - STRPOS(primary_poc, ' ')) AS last_name
+	FROM accounts;
+	
+/*
+Now see if you can do the same thing for every rep name in the sales_reps table. 
+Again provide first and last name columns.
+*/
+SELECT LEFT(name, STRPOS(name, ' ') -1) AS first_name,
+		RIGHT(name, LENGTH(name) - STRPOS(name, ' ')) AS last_name
+	FROM sales_reps;
+
+-- CONCAT & PIPING
+-- Example:
+/*
+SELECT first_name,
+		last_name,
+		CONCAT(first_name, ' ', last_name) AS full_name
+	FROM customer_data;
+*/
+
+-- Question:
+/*
+Each company in the accounts table wants to create an email address for each primary_poc. 
+The email address should be the first name of the primary_poc . last name primary_poc @ company name .com.
+*/
+WITH t1 AS (SELECT primary_poc, 
+					name,
+					LEFT(primary_poc, STRPOS(primary_poc, ' ') -1) AS first_name,
+					RIGHT(primary_poc, LENGTH(primary_poc) - STRPOS(primary_poc, ' ')) AS last_name
+				FROM accounts)
+SELECT CONCAT(first_name, '.', last_name, '@', name, '.com') AS email_addresss
+	FROM t1;
+
+/*
+You may have noticed that in the previous solution some of the company names include spaces, 
+which will certainly not work in an email address. See if you can create an email address that will work by removing all of the spaces in the account name, 
+but otherwise your solution should be just as in question 1. Some helpful documentation is here(opens in a new tab).
+*/
+
+
+/*
+We would also like to create an initial password, which they will change after their first log in. 
+The first password will be the first letter of the primary_poc's first name (lowercase), 
+then the last letter of their first name (lowercase), the first letter of their last name (lowercase), 
+the last letter of their last name (lowercase), the number of letters in their first name, 
+the number of letters in their last name, and then the name of the company they are working with, all capitalized with no spaces.
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
